56db59377c5c9ffcb0fd35c178aad0bcb0606707
Alfonso Suarez <luisalfonso2297@gmail.com>
2020-07-30 23:38:02 -0500

    Agrega el frec.py

---
+++ frec.py
@@ -0,0 +1,23 @@
+# Para ejecutarlo solo necesitas tener Pandas, lo instalas con
+# python -m pip install pandas
+# La ejecución del programa genera un HTML con una tabla en la que 
+# puedes ver la frecuencia de las letas.
+# Psdt: Aún no soporta caracteres extraños :(
+
+from string import ascii_lowercase
+from collections import Counter
+
+
+
+with open('report.txt') as f:
+    a = Counter(letter for line in f 
+                  for letter in line.lower() 
+                  if letter in ascii_lowercase)
+
+print(a)
+
+import pandas as pd
+df = pd.DataFrame.from_dict(a, orient='index').reset_index()
+df = df.rename(columns={'index':'Word', 0:'Count'})
+#print (df)
+df.to_html(open('SingleWord.html', 'w'))
